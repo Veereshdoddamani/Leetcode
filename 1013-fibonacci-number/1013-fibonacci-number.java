b740a1f720954f8056a4af196077e11cd6961e11
Veeresh Doddamani <151776533+Veereshdoddamani@users.noreply.github.com>
2025-10-25 14:28:56 +0530

    Time: 0 ms (100%), Space: 40.8 MB (19.04%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -6,6 +6,13 @@ class Solution {
         if(n==1){
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
     }
 }
